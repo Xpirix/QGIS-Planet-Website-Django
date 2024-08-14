2fa0027afc4bcc1a36a948bb7cd204b36a26b2b8
Lova ANDRIARIMALALA <43842786+Xpirix@users.noreply.github.com>
2024-08-14 11:06:29 +0300

    Add navigation top bar

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -2,14 +2,13 @@ import React from 'react';
 
 const Header: React.FC = () => {
   return (
-    <header className="hero is-primary">
-      <div className="hero-body">
-        <div className="container has-text-centered">
-          <h1 className="title">My Website</h1>
-          <h2 className="subtitle">Welcome to my website built with React and Bulma</h2>
-        </div>
-      </div>
-    </header>
+    <section style={{height: 60 + 'px'}}>
+        <qg-top-nav
+        breakpoint="1024"
+        class="is-highest navbar is-fixed-top"
+        location-prefix="https://qgis.org"
+    ></qg-top-nav>
+    </section>
   );
 };
 
